54582de1e01a1df7a3c17aedf52161b0fe15f6a0
janubh <152502479+janubh@users.noreply.github.com>
2023-12-04 10:19:33 +0000

    done this

--- 01_basics/01-variables.js
+++ 01_basics/01-variables.js
@@ -1,1 +1,18 @@
+const accountId =12345
+let accountEmail= "janvi@gmail.com"
+var accountpassword ="54321"
+accountcity= "Gujarat"
+let accountstate 
 
+//  accountId= 2 // not allowed
+
+accountEmail= "jaas@gmail.com"
+accountpassword ="9876"
+accountcity= "jaipur"
+//console.log(accountEmail);//
+//console.log(accountpassword);//
+//console.log(accountcity);//
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate])
+
+console.log(accountId);
