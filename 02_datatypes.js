7f2293aecb43ad4dd114b59f3e748e3dd9f491f0
janubh <152502479+janubh@users.noreply.github.com>
2023-12-04 10:55:08 +0000

    done

---
+++ 02_datatypes.js
@@ -0,0 +1,23 @@
+"us strict;"// newly js all code//
+
+//alert(3 +3)//
+
+
+let name="janvi"
+let age="21"
+let isloggedIn = false
+
+//number =>2 to power 53
+//biginit longa numeric 
+//string=>""
+//bollean=>true/false
+//null =>standloan value
+// undefined=
+//symbol =>unique
+
+
+//object
+
+
+
+console.log(typeof "janvi");
